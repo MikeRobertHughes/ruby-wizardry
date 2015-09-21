52500d8c562d0c86c253bb0a5d6f95623e3ccb51
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-09-20 21:31:28 -0500

    Complete Ruby Wizardry book and all exercises

---
+++ ch8/monkey_2.rb
@@ -0,0 +1,13 @@
+class Monkey
+  @@number_of_monkeys = 0
+
+  def initialize
+    @@number_of_monkeys += 1
+  end
+
+  class << self
+    def number_of_monkeys
+      @@number_of_monkeys
+    end
+  end
+end
---
+++ ch8/weezard_2.rb
@@ -0,0 +1,17 @@
+class Weezard
+
+  def initialize(name, power= 'Flight')
+    @name = name
+    @power = power
+    @spells = 5
+  end
+
+  def cast_spell(name)
+    if @spells > 0
+      @spells -= 1
+      puts "Cast #{name}! Spells left: #{@spells}."
+    else
+      puts 'No more spells!'
+    end
+  end
+end
